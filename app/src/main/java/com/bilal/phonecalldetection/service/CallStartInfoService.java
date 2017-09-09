c6d66d18aa0174d043e60acf15946bf37d306b00
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-09-09 19:26:49 +0530

    Added drag and drop to popup view

--- app/src/main/java/com/bilal/phonecalldetection/service/CallStartInfoService.java
+++ app/src/main/java/com/bilal/phonecalldetection/service/CallStartInfoService.java
@@ -7,10 +7,13 @@ import android.os.IBinder;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bilal.phonecalldetection.MainActivity;
 import com.bilal.phonecalldetection.R;
 
 public class CallStartInfoService extends Service {
@@ -22,6 +25,7 @@ public class CallStartInfoService extends Service {
     private WindowManager mWindowManager;
     private View mCallPopupView;
     private TextView mPhoneNumberTextView;
+    private LinearLayout mLinearLayout;
 
 
     public CallStartInfoService() {
@@ -36,6 +40,7 @@ public class CallStartInfoService extends Service {
         mCallPopupView = LayoutInflater.from(this).inflate(R.layout.serivce_call_start_popup, null);
         // Reference textview in popup layout
         mPhoneNumberTextView = (TextView) mCallPopupView.findViewById(R.id.textview_incoming_call_number_CallStartIntentService);
+        mLinearLayout = (LinearLayout) mCallPopupView.findViewById(R.id.linear_layout_CallStartInfoService);
 
         //Add the view to the window.
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
@@ -60,6 +65,58 @@ public class CallStartInfoService extends Service {
         //Add the view to the window
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mWindowManager.addView(mCallPopupView, params);
+
+
+        mLinearLayout.setOnTouchListener(new View.OnTouchListener() {
+            private int lastAction;
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        //remember the initial position.
+                        initialX = params.x;
+                        initialY = params.y;
+
+                        //get the touch location
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+
+                        lastAction = event.getAction();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        //As we implemented on touch listener with ACTION_MOVE,
+                        //we have to check if the previous action was ACTION_DOWN
+                        //to identify if the user clicked the view or not.
+                        if (lastAction == MotionEvent.ACTION_DOWN) {
+                            //Open the chat conversation click.
+                            Intent intent = new Intent(CallStartInfoService.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+
+                            //close the service and remove the chat heads
+                            stopSelf();
+                        }
+                        lastAction = event.getAction();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+
+                        //Update the layout with new X & Y coordinate
+                        mWindowManager.updateViewLayout(mCallPopupView, params);
+                        lastAction = event.getAction();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
